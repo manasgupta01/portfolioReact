fabec5b757314e531b79949e53fa314d8584eacb
manasgupta01 <manasgupta7624@gmail.com>
2023-07-18 02:06:49 +0530

    first commit

---
+++ src/tesst.js
@@ -0,0 +1,23 @@
+import React from 'react';
+  
+const tesst = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'Right',
+        alignItems: 'Right',
+        height: '100vh'
+      }}
+    >
+      <h1>Sign Up</h1>
+    </div>,
+    <div>
+      <h1>
+        hello
+      </h1>
+    </div>
+  );
+};
+  
+export default tesst;
